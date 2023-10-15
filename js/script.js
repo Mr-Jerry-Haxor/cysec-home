3df69bd8834670e24c225dde954f486d79c3913c
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-10-16 00:28:55 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+  autoplay: {
+    delay: 1000, // Delay in milliseconds between slide transitions
+    disableOnInteraction: false, // Keep autoplaying even when user interacts with swiper
+  },
+});
